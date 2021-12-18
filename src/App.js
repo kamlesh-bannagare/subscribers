9d4eeb2cf14becf6db90f5fc69148dd8b7cc88e2
kamlesh bannagare <kamleshbannagare10@gmail.com>
2021-12-18 16:52:17 +0530

    added component with dialog box

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 
 import './App.css';
+import { useState } from 'react'
 import AddSubscriber from './components/subscribers/AddSubscriber';
+import SubscriberList from './components/subscribers/SubscriberList';
 import Container from './components/templates/Container'
 
 
 function App() {
+  const [subscriberList, setSubscriberList] = useState([])
+  const onAddSubscriberHandler = (sname, spincode) => {
+    setSubscriberList((prevState) => {
+      return [...prevState, { name: sname, pincode: spincode, id: Math.random().toString() }]
+    })
+  }
   return (
     <div className="App">
-    
-     <AddSubscriber />
-    
+      <Container>
+        <AddSubscriber onAddSubscriber={onAddSubscriberHandler} />
+        <SubscriberList list={subscriberList} />
+      </Container>
     </div>
   );
 }
--- src/components/subscribers/AddSubscriber.js
+++ src/components/subscribers/AddSubscriber.js
@@ -2,9 +2,12 @@ import "./AddSubscriber.css";
 import Container from "../templates/Container";
 import Button from "../templates/Button";
 import { useState } from "react";
-const AddSubscriber = () => {
+import ErrorModal from '../templates/ErrorModal'
+
+const AddSubscriber = (props) => {
     const [name, setName] = useState("")
     const [pincode, setPincode] = useState("")
+    const [error, setError] = useState()
     const onNameChange = (events) => {
         console.log(events.target.value)
         setName(events.target.value)
@@ -15,16 +18,39 @@ const AddSubscriber = () => {
     }
     const onSubmitHandler = (events) => {
         events.preventDefault();
+        if (name.trim().length === 0) {
+            console.log("name is not valid")
+            setError({ title: "invalid name", content: "name is mandatory to get your desired reasult." })
+            return
+        }
+        if (pincode.trim().length === 0) {
+            console.log("pincode is not valid")
+            setError({ title: "invalid Pincode", content: "pincode is mandatory to get your desired reasult." })
+            return
+        }
+        if (pincode.length < 5 || pincode < 0) {
+            console.log("please ender valid pincode thad should be more than 4 digit")
+            setError({ title: "invalid Pincode", content: "please ender valid pincode thad should be more than 4 digit" })
+            return
+        }
         console.log("updated value", name, pincode)
+        props.onAddSubscriber(name, pincode)
+        setName('')
+        setPincode('')
+    }
+    const onCloseHandler=()=>{
+        setError(null)
     }
     return (
         <div>
+            {error && <ErrorModal title={error.title} content={error.content} onClose={onCloseHandler}/>}
+
             <form onSubmit={onSubmitHandler}>
                 <Container className="input">
                     <label htmlFor="name">Name</label>
-                    <input id="name" type="text" onChange={onNameChange}></input>
+                    <input value={name} id="name" type="text" onChange={onNameChange}></input>
                     <label htmlFor="pincode">Pincode</label>
-                    <input id="pincode" type="number" onChange={onPincodeChange}></input>
+                    <input value={pincode} id="pincode" type="number" onChange={onPincodeChange}></input>
                     <Button type="submit">Save</Button>
                 </Container>
             </form>
